7c3d1b15317eb6e3d53b98415e6bb3380f020ea9
Eduard Larionov <vesh95.17@ya.ru>
2026-03-29 02:13:15 +0500

    impl interface

--- internal/actioninfo/actioninfo.go
+++ internal/actioninfo/actioninfo.go
@@ -1,9 +1,26 @@
 package actioninfo
 
+import (
+	"fmt"
+)
+
 type DataParser interface {
-	// TODO: добавить методы
+	Parse(datastring string) error
+	ActionInfo() (string, error)
 }
 
 func Info(dataset []string, dp DataParser) {
-	// TODO: реализовать функцию
+	for _, v := range dataset {
+		err := dp.Parse(v)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		info, err := dp.ActionInfo()
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		fmt.Print(info)
+	}
 }
